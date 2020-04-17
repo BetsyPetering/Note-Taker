df9fd8d4023cecbd63e75c909e2da9298fcdcb04
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-04-16 19:48:41 -0700

    Create and Read work

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,9 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
+var fs = require("fs");
 
 // ===============================================================================
 // ROUTING
@@ -35,25 +33,21 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+  app.post("/api/notes", function(req, res) {
+    // console.log(req);
+    var db = fs.readFileSync("db/db.json");
+
+    var noteArray = JSON.parse(db);
+
+    console.log(noteArray);
+    
   });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/notes/:id", function(req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
     waitListData.length = 0;
